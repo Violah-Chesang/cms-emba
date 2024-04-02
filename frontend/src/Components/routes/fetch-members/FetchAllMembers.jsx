0d45ff8bfbc63865d58e84207936d7130bb76ce0
Violah Chesang <chesangv21@gmail.com>
2024-04-02 11:10:20 +0300

    Created all members page and its functional component

---
+++ frontend/src/Components/routes/fetch-members/FetchAllMembers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FetchAllMembers() {
+  return (
+    <div className='all-members'>
+        <h1>All Members</h1>
+    </div>
+  )
+}
+
+export default FetchAllMembers;
