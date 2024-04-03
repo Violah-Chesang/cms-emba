2a0d378ec0dd1dfb7a35e0bfae963752d780e550
Violah Chesang <chesangv21@gmail.com>
2024-04-03 22:30:56 +0300

    Added comments to deleteMember page

--- frontend/src/Components/routes/deleteMember/DeleteMember.jsx
+++ frontend/src/Components/routes/deleteMember/DeleteMember.jsx
@@ -2,9 +2,13 @@ import axios from 'axios';
 import React from 'react';
 
 function DeleteMember(props) {
+    //accepts id as props from parent(FetchMember)component
     const id = props.id;
-    console.log(`Delete member id ${id}`);
+
+    // function to run when delete icon is clicked
     const handleDelete = async() => {
+        
+        // uses object id to search for a member
         const res = await axios.post('http://localhost:5500/member/delete',{id});
         const data = res.data;
         alert(`Member deleted successfully! Deleted:${data.deleted}!!`);
