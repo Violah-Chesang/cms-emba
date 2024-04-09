f2ce47b08dc5ed405c9956a99ebfb3fc591de860
Violah Chesang <chesangv21@gmail.com>
2024-04-09 18:02:29 +0300

    Created the Not Found page for when a user enters the wrong address

---
+++ frontend/src/Components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <>
+        <h1>Page Not Found</h1>      
+        <p>Please enter a valid page address!</p>
+    </>
+  );
+}
+
+export default NotFound;
