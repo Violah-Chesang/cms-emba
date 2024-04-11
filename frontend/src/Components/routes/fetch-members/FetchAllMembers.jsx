29f91054f13efe7d113c17aac52e94142373f887
Violah Chesang <chesangv21@gmail.com>
2024-04-11 17:01:29 +0300

    Imported css page

--- frontend/src/Components/routes/fetch-members/FetchAllMembers.jsx
+++ frontend/src/Components/routes/fetch-members/FetchAllMembers.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import './fetchAllMembers.css';
 
 function FetchAllMembers() {
   const [members, setMembers] = useState([]);
